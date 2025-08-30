0113398e4357e13360c70cde34ff17a49c02f7dd
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-30 12:45:57 +0500

    ok

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function toggleMenu() {
       document.getElementById("nav-links").classList.toggle("active");
     }
+     let slideIndex = 0;
+    const slider = document.getElementById("slider");
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+      if (index >= totalSlides) {
+        slideIndex = 0;
+      } else if (index < 0) {
+        slideIndex = totalSlides - 1;
+      }
+      slider.style.transform = `translateX(${-slideIndex * 100}%)`;
+    }
+
+    function moveSlide(step) {
+      slideIndex += step;
+      showSlide(slideIndex);
+    }
+
+    // Auto-slide every 3 seconds
+    setInterval(() => {
+      moveSlide(1);
+    }, 3000);
